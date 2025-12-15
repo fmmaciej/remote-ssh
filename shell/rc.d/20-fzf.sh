031b65d89ac5a9fefc2efe2cf19563a747a7fe31
fm <haj.maciek@gmail.com>
2025-12-15 16:16:00 +0100

    Add fzf-history

--- shell/rc.d/20-fzf.sh
+++ shell/rc.d/20-fzf.sh
@@ -2,7 +2,29 @@
 
 ensure_this_file_sourced
 
-if have fzf; then
-  : "${FZF_DEFAULT_OPTS:=--height=40% --reverse --border}"
-  export FZF_DEFAULT_OPTS
+have fzf || return 0
+
+: "${FZF_DEFAULT_OPTS:=--height=40% --reverse --border}"
+export FZF_DEFAULT_OPTS
+
+__fzf_history() {
+  local cmd
+
+  cmd=$(
+    history \
+      | sed 's/^[[:space:]]*[0-9]\+[[:space:]]*//' \
+      | awk '!seen[$0]++' \
+      | fzf \
+          --prompt='history> ' \
+          --preview 'echo {}' \
+          --preview-window=down:3:wrap
+  ) || return
+
+  READLINE_LINE="$cmd"
+  READLINE_POINT=${#READLINE_LINE}
+}
+
+# bind tylko jeśli readline (bash interaktywny)
+if [[ -n "${BASH_VERSION:-}" ]]; then
+  bind -x '"\C-r":__fzf_history'
 fi

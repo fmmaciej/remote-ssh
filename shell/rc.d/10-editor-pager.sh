dda955296abc71270ab1e4399fefc084a9432a28
fm <haj.maciek@gmail.com>
2026-04-21 12:09:42 +0200

    fix(vim): errors regarding nvim

--- shell/rc.d/10-editor-pager.sh
+++ shell/rc.d/10-editor-pager.sh
@@ -4,10 +4,22 @@ ensure_this_file_sourced
 
 if have nvim; then
   export EDITOR="nvim"
+  export VISUAL="nvim"
 elif have vim; then
   export EDITOR="vim"
+  export VISUAL="vim"
 else
-  export EDITOR="vi"
+  unset EDITOR
+  unset VISUAL
+
+  case $- in
+    *i*)
+      if [[ -z "${REMOTE_SSH_EDITOR_WARNED:-}" ]]; then
+        printf '[WARN] No vim/nvim found in PATH. Install one manually if you want an editor.\n' >&2
+        export REMOTE_SSH_EDITOR_WARNED=1
+      fi
+      ;;
+  esac
 fi
 
 if have bat; then
